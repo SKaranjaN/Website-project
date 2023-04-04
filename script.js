87b7b210ce9083ec278b4a551c482c00ac857328
Stephen <skaranja654@gmail.com>
2023-04-04 20:29:09 +0300

    Completed assignment

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const h2 = document.createElement("h2");
 h2.textContent = "";
 document.querySelector("body").appendChild(h2);
 // start
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
